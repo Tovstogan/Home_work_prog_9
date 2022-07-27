be67fc247c24db11c195fcd9660c378374a4a185
Tovstogan <andrey-tovstogan@yandex.ru>
2022-07-27 16:31:11 +0400

    Задача_64

---
+++ Задача_64/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+M = 4; N = 8. -> ""4, 6, 7, 8""*/ 
+
+Console.WriteLine();
+Console.WriteLine("Задача 64");
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while (true)
+    {
+
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return result;
+}
+string recursion(int m, int n)
+{
+if(n == m)
+return m.ToString();
+else
+return recursion(m,n-1) + ", "+ n;
+}
+
+int m = GetNumber("Введите число M:");
+int n = GetNumber("Введите число N:");
+Console.Write($"{recursion(m,n)}");
